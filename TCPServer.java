6eb3f387507692d5908d8ef4ee98124d53478b3b
PatrickCTRF <patrickctrf@hotmail.com.br>
2016-10-11 01:46:09 -0300

    Update TCPServer.java

--- TCPServer.java
+++ TCPServer.java
@@ -6,23 +6,27 @@ import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-class TCPServer
-{
-    public static void main(String argv[]) throws Exception
-    {
-        String clientSentence;
-        String capitalizedSentence;
-        ServerSocket welcomeSocket = new ServerSocket(6789);
 
-        while(true)
+class TCPServer {
+        public static void main(String argv[]) throws Exception
         {
-            Socket connectionSocket = welcomeSocket.accept();
-            BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
-            OutputStream outToClient = connectionSocket.getOutputStream();
-            clientSentence = inFromClient.readLine();
-            System.out.println("Received: " + clientSentence);
-            capitalizedSentence = "Eu sou servidor\n";
-            outToClient.write(capitalizedSentence.getBytes());
+            String clientSentence = "", aux;
+            String capitalizedSentence;
+            ServerSocket welcomeSocket = new ServerSocket(6789);
+
+            while(true)
+            {   
+                clientSentence = "";
+                Socket connectionSocket = welcomeSocket.accept();
+                BufferedReader inFromClient =
+                        new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+                OutputStream outToClient = connectionSocket.getOutputStream();
+                while(!(aux = inFromClient.readLine()).equals("FIM")){
+                    clientSentence += aux + "\n";
+                }
+                System.out.println("Received: " + clientSentence);
+                capitalizedSentence = "Eu sou servidor\n";
+                outToClient.write(capitalizedSentence.getBytes());
+            }
         }
-    }
 }
